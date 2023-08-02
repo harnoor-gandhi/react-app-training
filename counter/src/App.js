15f69fb0eb4409ba9500d957fb767dbd21478939
harnoor-gandhi <56862515+harnoor-gandhi@users.noreply.github.com>
2023-08-02 11:47:17 +0530

    Update App.js

--- counter/src/App.js
+++ counter/src/App.js
@@ -1,11 +1,12 @@
 // import logo from './logo.svg';
 import './App.css';
 import State from './state_file'
+import ColorChanger from './conditionals'
 function App() {
   return (
     <div className="App">
        <State />
-       
+      <ColorChanger/> 
     </div>
   );
 }
